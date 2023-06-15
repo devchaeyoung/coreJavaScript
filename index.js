e2c3fa40ff09fc58db99de7b171b1f0c93dc4f72
chaeyoung <chaemaa00@gmail.com>
2023-06-15 16:57:56 +0900

    230615

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const array = [1, 2, 3, 4, 5];
+// const array = [1, 2, 3, 4, 5];
 
-function printNum(num) {
-  console.log(num);
-}
+// function printNum(num) {
+//   console.log(num);
+// }
 
-array.forEach(printNum);
+// array.forEach(printNum);
+var arr = [1, 2];
+Array.prototype.toString.call(arr); //(1)
+Object.proptotype.toString.call(arr); //(2)
+arr.toString(); //1,2
+
+arr.toString = function () {
+  return this.join("_");
+};
+
+arr.toString();
+const newArr = arr.toString();
+console.log(newArr);
